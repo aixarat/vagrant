0fb1365eb448a27951b643247e3b5ef3fcc22f85
Vlad H <vladimir@bajalibre.com>
2015-12-07 03:23:15 +0800

    corrected typo

--- scripts/centos-database.sh
+++ scripts/centos-database.sh
@@ -2,7 +2,7 @@
 
 
 # php and mysql
-yum install -y -mysql mysql mysql-server mysql-devel
+yum install -y mysql mysql-server mysql-devel
 chkconfig --add mysqld
 chkconfig mysqld on
 service mysqld start
